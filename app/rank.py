e54a1214d9eb35ae64220bbf5b7f94e6adde1e84
jzc <985811440@qq.com>
2019-03-17 15:43:28 +0800

    build 7 finish rank ,update functions todo: add lib, password, likes

--- app/rank.py
+++ app/rank.py
@@ -1,8 +1,8 @@
+# call Update function before rank !!!
 from app import api
 from flask import request, jsonify
 from app.models import *
 import redis
-from datetime import date
 
 redis_db = redis.StrictRedis(host="127.0.0.1", port=6379, db=1)
 
@@ -28,9 +28,9 @@ def step_person():
     offset = int(request.args.get('offset') or 0)
     limit = int(request.args.get('limit') or 10)
     data = list()
-    for uid in redis_db.zrevrange('step_person_rank', offset, offset + limit):
+    for uid, step in redis_db.zrevrange('step_person_rank', offset, offset + limit, withscores=True):
         student = Student.query.get(uid)
-        step = WeRun.query.filter_by(user_id=student.id, time=date.today().isoformat()).first().step
+        # step = WeRun.query.filter_by(user_id=student.id, time=date.today().isoformat()).first().step
         data.append({
             'step': step,
             'user_id': student.id,
@@ -49,5 +49,31 @@ def dept_today():
             "department_id": dept_id,
             "department_name": Department.query.get(dept_id).department_name,
             "likes": Likes_dep_daily.query.filter_by(star_id=dept_id).count()
+        })
+    return jsonify(data), 200
+
+
+@api.route('/rank/step/dept/week')
+def dept_week():
+    data = []
+    for dept_id, step in redis_db.zrevrange('dep_weekly_rank', 0, -1, withscores=True):
+        data.append({
+            "step": step,
+            "department_id": dept_id,
+            "department_name": Department.query.get(dept_id).department_name,
+            "likes": Likes_dep_weekly.query.filter_by(star_id=dept_id).count()
+        })
+    return jsonify(data), 200
+
+
+@api.route('/rank/step/dept/month')
+def dept_month():
+    data = []
+    for dept_id, step in redis_db.zrevrange('dep_monthly_rank', 0, -1, withscores=True):
+        data.append({
+            "step": step,
+            "department_id": dept_id,
+            "department_name": Department.query.get(dept_id).department_name,
+            "likes": Likes_dep_monthly.query.filter_by(star_id=dept_id).count()
         })
     return jsonify(data), 200
--- app/update.py
+++ app/update.py
@@ -1,6 +1,6 @@
+# call Update function before rank !!!
 from app.rank import *
-import json
-from datetime import date
+from datetime import date, timedelta
 
 
 # 图书馆排行
@@ -13,13 +13,16 @@ def lib_rank():
 # 个人步数日排行
 def step_person_rank():
     redis_db.delete('step_person_rank')
+    # WIP clear likes
     for student in Student.query.all():
-        step = WeRun.query.filter_by(user_id=student.id, time=date.today().isoformat()).first().step
-        redis_db.zadd('step_person_rank', {student.id: step})
+        werun = WeRun.query.filter_by(user_id=student.id, time=date.today().isoformat()).first()
+        if werun:
+            redis_db.zadd('step_person_rank', {student.id: werun.step})
 
 
 # 学院步数日排行
 def dep_daily_rank():
+    # WIP clear likes
     redis_db.delete('dep_daily_rank')
     # init data structure
     data = []
@@ -27,8 +30,6 @@ def dep_daily_rank():
         data.append({
             "step": 0,
             "department_id": dept.id,
-            "department_name": dept.department_name,
-            "likes": 0,
             "count": 0,
         })
     for werun in WeRun.query.filter_by(time=date.today().isoformat()):
@@ -37,10 +38,48 @@ def dep_daily_rank():
         data[student.department_id - 1]['count'] += 1
     # calc avg step
     for item in data:
-        item['count'] = item['count'] or 1 # lest divided by zero
+        item['count'] = item['count'] or 1  # lest divided by zero
         item['step'] = int(item['step'] / item['count'])
         redis_db.zadd('dep_daily_rank', {item['department_id']: item['step']})
 
-    # for student in Student.query.all():
-    #     step = WeRun.query.filter_by(user_id=student.id, time=date.today().isoformat()).first().step
-    #     redis_db.zadd('dep_daily_rank', {student.id: step}
+# 学院步数周排行
+def dep_weekly_rank():
+    # WIP clear likes
+    redis_db.delete('dep_weekly_rank')
+    # init data structure
+    data = []
+    for dept in Department.query.all():
+        data.append({
+            "step": 0,
+            "department_id": dept.id,
+        })
+    end = date.today().isoformat()
+    start = (date.today() - timedelta(days=7)).isoformat()
+    # calc total step
+    for werun in WeRun.query.filter(WeRun.time >= start, WeRun.time <= end):
+        student = Student.query.get(werun.user_id)
+        data[student.department_id - 1]['step'] += werun.step
+    # add redis
+    for item in data:
+        redis_db.zadd('dep_weekly_rank', {item['department_id']: item['step']})
+
+# 学院步数周排行
+def dep_monthly_rank():
+    # WIP clear likes
+    redis_db.delete('dep_monthly_rank')
+    # init data structure
+    data = []
+    for dept in Department.query.all():
+        data.append({
+            "step": 0,
+            "department_id": dept.id,
+        })
+    end = date.today().isoformat()
+    start = (date.today() - timedelta(days=30)).isoformat()
+    # calc total step
+    for werun in WeRun.query.filter(WeRun.time >= start, WeRun.time <= end):
+        student = Student.query.get(werun.user_id)
+        data[student.department_id - 1]['step'] += werun.step
+    # add redis
+    for item in data:
+        redis_db.zadd('dep_monthly_rank', {item['department_id']: item['step']})
