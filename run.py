f9a63a89a6648d93341d38462133ccc63167db58
jzc <985811440@qq.com>
2019-03-22 16:48:21 +0800

    shadow pwd

--- run.py
+++ run.py
@@ -5,10 +5,12 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 
+if not os.environ.get('SQLALCHEMY_DATABASE_URI'):
+    raise ValueError('SQLALCHEMY_DATABASE_URI')
 base_dir = os.path.abspath(os.path.dirname(__name__))
 app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY') or b'apple'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Q110110110@localhost/rank'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
