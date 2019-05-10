98f62d35527d6d1390a673b26ef4bc5af54180f9
jzc <985811440@qq.com>
2019-05-10 21:29:52 +0800

    hide my password

--- run.py
+++ run.py
@@ -1,17 +1,14 @@
-import os, sys
+import os
+import sys
 from flask import Flask
 from app import *
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-# import time
-
-# time.sleep(10)
-
 
 base_dir = os.path.abspath(os.path.dirname(__name__))
 app = Flask(__name__)
-app.secret_key = os.environ.get('SECRET_KEY') or b'apple'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI') or 'mysql://jzc:Q110110110@127.0.0.1/rank?charset=utf8mb4'
+app.secret_key = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
